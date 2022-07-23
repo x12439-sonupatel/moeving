3b320d5403c17f63e300ae3ed5cfb81121bc5fd7
x12439-sonupatel <83572692+x12439-sonupatel@users.noreply.github.com>
2022-07-23 21:40:02 +0530

    change in home screen

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # moeving
+
+1. git clone https://github.com/x12439-sonupatel/moeving.git
+2. cd moeving
+3. yarn install
+4. react-native start --reset-cache
--- src/component/HomeScreen.js
+++ src/component/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import {
   Text,
   TouchableOpacity,
@@ -7,11 +7,11 @@ import {
   FlatList,
   StyleSheet,
   Image,
-} from 'react-native';
-import {deleteData, db} from '../firebase';
-import {editIcon, deleteIcon} from '../Images';
-const ref = db.collection('todoList');
-export default HomeScreen = ({navigation}) => {
+} from "react-native";
+import { deleteData, db } from "../firebase";
+import { editIcon, deleteIcon } from "../Images";
+const ref = db.collection("todoList");
+export default (HomeScreen = ({ navigation }) => {
   let [data, setData] = useState([]);
   let [extraData, setExtraData] = useState(null);
   let [isLoading, setLoading] = useState(false);
@@ -19,9 +19,9 @@ export default HomeScreen = ({navigation}) => {
   const fetchData = () => {
     setLoading(true);
     try {
-      ref.onSnapshot(querySnapshot => {
+      ref.onSnapshot((querySnapshot) => {
         const list = [];
-        querySnapshot.forEach(doc => {
+        querySnapshot.forEach((doc) => {
           if (doc && doc.exists) {
             list.push(doc.data());
           }
@@ -38,9 +38,9 @@ export default HomeScreen = ({navigation}) => {
     fetchData();
   }, []);
 
-  const deleteDataM = async ({id}) => {
+  const deleteDataM = async ({ id }) => {
     setLoading(true);
-    await deleteData({table: 'todoList', id});
+    await deleteData({ table: "todoList", id });
     setLoading(false);
     setExtraData(id);
   };
@@ -53,59 +53,64 @@ export default HomeScreen = ({navigation}) => {
         </View>
       ) : null}
 
-      {data && data.length ? (
-        <FlatList
-          data={data}
-          extraData={extraData}
-          renderItem={({item}) => {
-            return (
-              <View style={styles.renderContainer}>
-                <View style={styles.renderBoxTextStyle}>
-                  <Text numberOfLines={1}>{item?.text}</Text>
+      {data && data?.length ? (
+        <>
+          <FlatList
+            data={data}
+            extraData={extraData}
+            renderItem={({ item }) => {
+              return (
+                <View style={styles.renderContainer}>
+                  <View style={styles.renderBoxTextStyle}>
+                    <Text numberOfLines={1}>{item?.text}</Text>
+                  </View>
+                  <TouchableOpacity
+                    onPress={() => {
+                      navigation.navigate("AddScreen", {
+                        ...item,
+                        name: "Edit Screen",
+                      });
+                    }}
+                  >
+                    <Image source={editIcon} />
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={{ paddingHorizontal: 10 }}
+                    onPress={() => deleteDataM(item)}
+                  >
+                    <Image source={deleteIcon} />
+                  </TouchableOpacity>
                 </View>
-                <TouchableOpacity
-                  onPress={() => {
-                    navigation.navigate('AddScreen', {
-                      ...item,
-                      name: 'Edit Screen',
-                    });
-                  }}>
-                  <Image source={editIcon} />
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={{paddingHorizontal: 10}}
-                  onPress={() => deleteDataM(item)}>
-                  <Image source={deleteIcon} />
-                </TouchableOpacity>
-              </View>
-            );
-          }}
-        />
+              );
+            }}
+          />
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => {
+                navigation.navigate("AddScreen", { name: "Add Screen" });
+              }}
+            >
+              <Text style={styles.buttonText}>Add Todo</Text>
+            </TouchableOpacity>
+          </View>
+        </>
       ) : null}
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => {
-            navigation.navigate('AddScreen', {name: 'Add Screen'});
-          }}>
-          <Text style={styles.buttonText}>Add Todo</Text>
-        </TouchableOpacity>
-      </View>
     </View>
   );
-};
+});
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
     borderRadius: 4,
     marginBottom: 20,
   },
   loaderStyle: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
+    justifyContent: "center",
+    alignItems: "center",
+    position: "absolute",
     top: 0,
     bottom: 0,
     left: 0,
@@ -113,7 +118,7 @@ const styles = StyleSheet.create({
   },
   renderBoxTextStyle: {
     borderWidth: 1,
-    borderColor: 'gray',
+    borderColor: "gray",
     paddingHorizontal: 10,
     marginHorizontal: 10,
     borderRadius: 4,
@@ -121,25 +126,25 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   renderContainer: {
-    flexDirection: 'row',
+    flexDirection: "row",
     marginTop: 10,
-    alignItems: 'center',
+    alignItems: "center",
   },
   buttonContainer: {
     height: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 10,
   },
   button: {
     borderWidth: 1,
-    borderColor: 'gray',
+    borderColor: "gray",
     borderRadius: 4,
     paddingHorizontal: 10,
     paddingVertical: 5,
-    backgroundColor: 'blue',
+    backgroundColor: "blue",
   },
   buttonText: {
-    color: 'white',
+    color: "white",
   },
 });
